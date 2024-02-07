0e291caf2ca22b1605af182791437ded46d16b64
mitchell_kimbell <mfkimbell@gmail.com>
2024-02-07 14:47:03 -0600

    just .tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 provider "aws" {
-region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "myec2" {
-ami = "ami-0c7217cdde317cfec"
-instance_type "t2.micro"
-tags {
-Name = "testEC2"
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "testEC2"
+  }
 }
